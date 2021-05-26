963e89e6fb1a320390e2220e0e80e6cbd6f2c574
Vanessa Diana <vanessa.d17@gmail.com>
2021-05-26 20:06:11 -0300

    aula 01

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,21 @@
+interface RepositoryItemItemProps {
+    repository:{
+        name: string;
+        description: string;
+        html_url: string;
+    }
+}
+
+export function RepositoryItem (props:RepositoryItemItemProps) {
+
+    return (
+        <li>
+            <strong>{props.repository.name?? 'Default'}</strong>
+            <p>{props.repository.description}</p>
+
+            <a href={props.repository.html_url}>
+                Acessar repositório
+            </a>
+        </li>
+    )
+}
